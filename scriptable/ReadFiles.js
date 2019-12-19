d3484035706b075420a987ed4f77456c192ab8e8
pugliathomas <puglia.thomas@gmail.com>
2019-12-19 12:27:15 -0600

    Create ReadFiles.js

---
+++ scriptable/ReadFiles.js
@@ -0,0 +1,22 @@
+// Fetch list of repos
+let secretKey = "" // Find in Working Copy app settings
+let url = "working-copy://x-callback-url/repos"
+let callback = new CallbackURL(url)
+callback.addParameter("key", secretKey)
+let result = await callback.open()
+let repos = JSON.parse(result.json)
+for (repo of repos) {
+  await listFileStatus(repo)
+}
+
+async function listFileStatus(repo) {
+  let url = "working-copy://x-callback-url/branches"
+  let callback = new CallbackURL(url)
+  callback.addParameter("repo", repo.name)
+  callback.addParameter("key", secretKey)
+  let result = await callback.open()
+  let branches = JSON.parse(result.json)
+  log(repo.name)
+  log(branches)
+  log("")
+}
